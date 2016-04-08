9b6e2421279ef281fb12c3ec0e913b0a10f135a9
robchiarelli <rob.chiarelli@nyu.edu>
2016-04-07 22:45:18 -0400

    added database initialization

--- Part1/groupdeal.py
+++ Part1/groupdeal.py
@@ -1,6 +1,7 @@
 # import stuff
 import sqlite3
 import os
+from contextlib import closing
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
 
 # create app
@@ -20,6 +21,12 @@ def home():
 
 def connect_db():
 	return sqlite3.connect(app.config['DATABASE'])
+	
+def init_db():
+	with closing(connect_db()) as db:
+		with app.open_resource('schema.sql', mode='r') as f:
+			db.cursor().executescript(f.read())
+		db.commit()
 	
 if __name__ == '__main__':
 	app.run()
