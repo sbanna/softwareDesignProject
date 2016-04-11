d24217c2c6f12a7152d93c485d3a0e831c7f9865
bas511 <bas511@nyu.edu>
2016-04-11 00:40:37 -0400

    fixed up schema. most things searchable using SQL

--- Part1/schema.sql
+++ Part1/schema.sql
@@ -10,7 +10,8 @@ CREATE TABLE user_account
 (
 	user_id 	int				NOT NULL 	PRIMARY KEY,
 	username 	varchar(255) 	NOT NULL,
-	password 	varchar(255) 	NOT NULL
+	password 	varchar(255) 	NOT NULL,
+	address		varchar(255)
 );
 
 CREATE TABLE vendor
@@ -24,7 +25,7 @@ CREATE TABLE product
 	product_id 	int 			NOT NULL 	PRIMARY KEY,
 	price 		int				NOT NULL,
 	image 		blob,
-	description varchar(255),
+	description varchar(1023s),
 	vendor_id	int				NOT NULL,
 	FOREIGN KEY(vendor_id) 		REFERENCES 	vendor(vendor_id)
 );
@@ -38,5 +39,16 @@ CREATE TABLE vendor_account
 CREATE TABLE consumer_account
 (
 	username 	varchar(255) 	NOT NULL,
-	FOREIGN KEY(username) 		REFERENCES 	user_account(username)
+	consumer_id int				NOT NULL,
+	FOREIGN KEY(username) 		REFERENCES 	user_account(username),
+	FOREIGN KEY(consumer_id)	REFERENCES  user_account(user_id)
+);
+
+CREATE TABLE contributions
+(
+	product_id  int        		NOT NULL,
+	consumer_id int             NOT NULL,
+	amount      numeric(8,2),
+	FOREIGN KEY(product_id)		REFERENCES product,
+	FOREIGN KEY(consumer_id)	REFERENCES consumer_account
 );
