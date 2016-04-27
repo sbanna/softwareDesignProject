27206b690adeb2283cc3f93fec1d96599047c9f0
robchiarelli <rob.chiarelli@nyu.edu>
2016-04-27 12:07:55 -0400

    Adding tempvariables.py

---
+++ Part1/tempvariables.py
@@ -0,0 +1,18 @@
+# information needed when viewing a single project
+campaign = {'title': 'The Campaign',
+			'author': 'John Doe',
+			'description': '100 character description or something of that sorts',
+			'currentPrice': 53.23,
+			'nextPrices': [50.00,45.00,42.00],
+			'pledgesNeeded' : [100, 150, 200, 250],
+			'mainImage': 'some image',
+			'percentFunded': 50,
+			};
+
+# information needed when viewing all active projects
+listofProjects = [
+			{'title': 'campaign1', 'author': 'author1', 'image':'image1', 'description': 'description1', 'currentPrice':'10', 'nextPrice':'20', 'commitsAttained':'12', 'commitsNeedNextPrice':'20'},
+			{'title': 'campaign2', 'author': 'author2', 'image':'image2', 'description': 'description2', 'currentPrice':'141', 'nextPrice':'150', 'commitsAttained':'16', 'commitsNeedNextPrice':'50'},
+			];
+
+#
